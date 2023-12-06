21b6a0148905fd76b7ea29a7dc5252d3506ad5fd
leandro4silva <leandro94.a.s@gmail.com>
2023-12-06 17:14:03 -0300

    fix: implement memberdata to category tests

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -1,4 +1,3 @@
-using Bogus;
 using FC.Codeflix.Catalog.Domain.Exceptions;
 using FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category;
 using FluentAssertions;
@@ -89,16 +88,24 @@ namespace FC.Codeflix.Catalog.UnitTests
         //nome deve ter no minimo 3 caracteres
         [Theory(DisplayName = nameof(InstantiateErrorWhenNameIsLessThan3Characters))]
         [Trait("Domain", "Category - Aggregates")]
-        [InlineData("1")]
-        [InlineData("12")]
-        [InlineData("a")]
-        [InlineData("ab")]
+        [MemberData(nameof(GetNamesWithLessThan3Characters), parameters: 6)]
         public void InstantiateErrorWhenNameIsLessThan3Characters(string invalidName)
         {
             var validCategory = _categoryTestFixture.GetValidCategory();
 
             Action action = () => new DomainEntity.Category(invalidName, validCategory.Description);
             action.Should().Throw<EntityValidationException>().WithMessage("Name should be at leats 3 characters long");
+        }
+
+        public static IEnumerable<object[]> GetNamesWithLessThan3Characters(int numberOfTests)
+        {
+            var fixture = new CategoryTestFixture();
+
+            for (int i = 0; i < numberOfTests; i++)
+            {
+                var isOdd = i % 2 == 1;
+                yield return new object[] { fixture.GetValidCategoryName()[..(isOdd ? 1 : 2)] };
+            }
         }
 
         //name deve ter no maximo 255 caracteres
