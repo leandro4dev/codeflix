5a5e6f98987028abf023fcca79e3b4ea57443001
leandro4silva <leandro94.a.s@gmail.com>
2023-12-06 16:36:34 -0300

    feat: refactoring and add bogus

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTestFixture.cs
@@ -1,12 +1,46 @@
+using FC.Codeflix.Catalog.UnitTests.Common;
 using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
 
 namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category
 {
-    public class CategoryTestFixture
+    public class CategoryTestFixture : BaseFixture
     {
+        public CategoryTestFixture() : base() { }
+
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+
+            while (categoryName.Length < 3)
+            {
+                categoryName = Faker.Commerce.Categories(1)[0];
+            }
+
+            if (categoryName.Length > 255)
+            {
+                categoryName = categoryName[..255]; //primeiros 255 characters
+            }
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription = Faker.Commerce.ProductDescription();
+            if (categoryDescription.Length > 10000)
+            {
+                categoryDescription = categoryDescription[..10_000];
+            }
+
+            return categoryDescription;
+        }
+
         public DomainEntity.Category GetValidCategory()
         {
-            return new DomainEntity.Category("Category name", "Category Description");
+            return new DomainEntity.Category(
+                GetValidCategoryName(),
+                GetValidCategoryDescription()
+            );
         }
     }
 
