dce24af674f9a26998c78360959602e75420f9b3
leandro4silva <leandro94.a.s@gmail.com>
2023-12-08 16:41:20 -0300

    feat: create generic repository interface

---
+++ src/FC.Codeflix.Catalog.Domain/SeedWork/IGenericRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace FC.Codeflix.Catalog.Domain.SeedWork;
+public interface IGenericRepository<TAgrregate> : IRepository
+{
+    public Task Insert(TAgrregate aggregate, CancellationToken cancellationToken);
+}
