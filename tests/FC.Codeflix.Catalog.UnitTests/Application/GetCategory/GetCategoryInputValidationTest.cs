21def7e7a8bed5fafef16fbee67f701679aff9e7
leandro4silva <leandro94.a.s@gmail.com>
2024-01-10 16:40:50 -0300

    feat: implements test on get category input

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidationTest.cs
@@ -0,0 +1,30 @@
+﻿
+using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidationTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryInputValidationTest(GetCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategoryInputValidationTest - UseCases")]
+    public void ValidationOk()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+}
