290cf8e387c1c5f5f22afc01f55377390be85c87
leandro4silva <leandro94.a.s@gmail.com>
2024-01-11 11:40:34 -0300

    feat: added fluent validation service

---
+++ src/FC.Codeflix.Catalog.Application/Validation/FluentvalidationService.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace FC.Codeflix.Catalog.Application.Validation;
+
+public class FluentValidationService<TValidationInput> : AbstractValidator<TValidationInput>
+{
+    public FluentValidationService()
+    {
+        ValidatorOptions.Global.LanguageManager.Enabled = false;
+    }
+}
