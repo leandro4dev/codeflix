aaf888bc0bd1fd1b73f49b5c2f5b6fe47189e5b4
leandro4silva <leandro94.a.s@gmail.com>
2024-01-11 11:41:43 -0300

    feat: refactoring to use fluent validation

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/DeleteCategory/DeleteCategoryInputValidator.cs
@@ -0,0 +1,12 @@
+﻿using FC.Codeflix.Catalog.Application.Validation;
+using FluentValidation;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+
+public class DeleteCategoryInputValidator : FluentValidationService<DeleteCategoryInput>
+{
+    public DeleteCategoryInputValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
--- src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
@@ -1,12 +1,12 @@
-﻿using FluentValidation;
+﻿using FC.Codeflix.Catalog.Application.Validation;
+using FluentValidation;
 
 namespace FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 
-public class GetCategoryInputValidator : AbstractValidator<GetCategoryInput>
+public class GetCategoryInputValidator : FluentValidationService<GetCategoryInput>
 {
     public GetCategoryInputValidator()
     {
-        ValidatorOptions.Global.LanguageManager.Enabled = false;
         RuleFor(x => x.Id).NotEmpty();
     }
 }
