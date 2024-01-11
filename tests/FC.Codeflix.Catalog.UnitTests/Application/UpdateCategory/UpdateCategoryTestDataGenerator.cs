6b3023fcf2a33c4de3e0936b0c5d91d14d2a60a6
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-11 20:50:04 -0300

    feat: refactoring test to use data generator

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -0,0 +1,27 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+public class UpdateCategoryTestDataGenerator
+{
+    public static IEnumerable<object[]> GetCategoriesToUpdate(int times = 5)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        for(int index = 0; index < times; index++)
+        {
+            var exampleCategory = fixture.GetExampleCategory();
+            var exampleInput = new UpdateCategoryInput(
+                exampleCategory.Id,
+                fixture.GetValidCategoryName(),
+                fixture.GetValidCategoryDescription(),
+                fixture.GetRandomBoolean()
+            );
+
+            yield return new object[]
+            {
+                exampleCategory,
+                exampleInput
+            };
+        }
+    }
+}
