24c2826ed738db829356302ebfc1864fdd13c132
leandro4silva <leandro94.a.s@gmail.com>
2024-01-11 17:28:28 -0300

    feat: implements update category tests

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,65 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FluentAssertions;
+using Moq;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(UpdateCategory))]
+    [Trait("Application", "UpdateCategory - Use Cases")]
+    public async Task UpdateCategory()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var categoryExample = _fixture.GetExampleCategory();
+
+        repositoryMock.Setup(x => x.Get(
+            categoryExample.Id,
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(categoryExample);
+
+        var input = new UpdateCategoryInput(
+            categoryExample.Id,
+            _fixture.GetValidCategoryName(), 
+            _fixture.GetValidCategoryDescription(), 
+            !categoryExample.IsActive
+        );
+
+        var useCase = new UseCase.UpdateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        //act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        //assert
+        repositoryMock.Verify(x => x.Get(
+            categoryExample.Id,
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+
+        repositoryMock.Verify(x => x.Update(
+            categoryExample,
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+
+        unitOfWorkMock.Verify(x => x.Commit(
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+
+   
+        output.Should().NotBeNull();
+        output.Id.Should().Be(input.Id);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,66 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+public class UpdateCategoryTestFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName[..255]; //primeiros 255 characters
+        }
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10000)
+        {
+            categoryDescription = categoryDescription[..10_000];
+        }
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean()
+    {
+        return (new Random()).NextDouble() < 0.5;
+    }
+
+    public Category GetExampleCategory()
+    {
+        return new Category(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+    }
+
+
+    public Mock<ICategoryRepository> GetRepositoryMock() 
+    { 
+        return new Mock<ICategoryRepository>();
+    }
+
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() 
+    { 
+        return new Mock<IUnitOfWork>();
+    }
+}
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection : ICollectionFixture<UpdateCategoryTestFixture> { }
