be15b4afa761b949660048d06c5664b041cf95bb
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-13 16:41:22 -0300

    feat: added list categories test fixture

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+public class ListCategoriesTestFixture : BaseFixture
+{
+
+}
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection :
+    ICollectionFixture<ListCategoriesTestFixture> { }
