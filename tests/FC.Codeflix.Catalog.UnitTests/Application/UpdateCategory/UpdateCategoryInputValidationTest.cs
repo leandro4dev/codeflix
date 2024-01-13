22d867a65986543b8e63b27fac11d14b68fdb18e
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-13 12:20:50 -0300

    feat: implements update category input validation test

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryInputValidationTest.cs
@@ -0,0 +1,44 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryInputValidationTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryInputValidationTest(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "UpdateCategoryInputValidationTest - UseCases")]
+    public void ValidationOk()
+    {
+        var validInput = _fixture.GetValidInput();
+        var validator = new UpdateCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+
+    [Fact(DisplayName = nameof(InvalidWhenEmptyGuidId))]
+    [Trait("Application", "GetCategoryInputValidationTest - UseCases")]
+    public void InvalidWhenEmptyGuidId()
+    {
+        var validInput = _fixture.GetValidInput(Guid.Empty);
+        var validator = new UpdateCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.Should().HaveCount(1);
+        validationResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+    }
+}
