76d7307c8e89a5c6c8eacf58e2b0932cb79c390e
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-13 12:20:15 -0300

    feat: implements update category input validator

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInputValidator.cs
@@ -0,0 +1,12 @@
+﻿using FC.Codeflix.Catalog.Application.Validation;
+using FluentValidation;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+public class UpdateCategoryInputValidator : FluentValidationService<UpdateCategoryInput>
+{
+    public UpdateCategoryInputValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
