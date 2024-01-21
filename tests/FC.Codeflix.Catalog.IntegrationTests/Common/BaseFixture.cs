bf123b0951fab88e74252ca6548e67ae17aa9b47
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-20 23:56:42 -0300

    feat: implements category repository tests

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Common/BaseFixture.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Common;
+
+public class BaseFixture
+{
+    protected Faker Faker { get; set; }
+
+    public BaseFixture()
+    {
+        Faker = new Faker("pt_BR");
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -0,0 +1,66 @@
+﻿using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.IntegrationTests.Common;
+using Microsoft.EntityFrameworkCore;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
+
+
+public class CategoryRepositoryTestFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName[..255]; //primeiros 255 characters
+        }
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10000)
+        {
+            categoryDescription = categoryDescription[..10_000];
+        }
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean()
+    {
+        return new Random().NextDouble() < 0.5;
+    }
+
+    public DomainEntity.Category GetExampleCategory()
+    {
+        return new DomainEntity.Category(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+    }
+
+    public CodeflixCatalogDbContext CreateDbContext()
+    {
+        return new CodeflixCatalogDbContext(
+            new DbContextOptionsBuilder<CodeflixCatalogDbContext>()
+            .UseInMemoryDatabase("integration-tests-db")
+            .Options
+        );
+    }
+}
+
+[CollectionDefinition(nameof(CategoryRepositoryTestFixture))]
+public class CategoryRepositoryTestFixtureColletion
+    : ICollectionFixture<CategoryRepositoryTestFixture>
+{ }
