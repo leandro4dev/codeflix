b0a6b4554cf46d120e96e7eadec007e6292da37b
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-20 23:57:25 -0300

    feat: implements category repository

---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly CodeflixCatalogDbContext _context;
+
+    public CategoryRepository(CodeflixCatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Insert(Category aggregate, CancellationToken cancellationToken)
+    {
+        await _context.AddAsync(aggregate, cancellationToken);
+    }
+
+
+    public Task Delete(Category aggregate, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> Get(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SearchOutput<Category>> Search(SearchInput input, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Category aggregate, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
