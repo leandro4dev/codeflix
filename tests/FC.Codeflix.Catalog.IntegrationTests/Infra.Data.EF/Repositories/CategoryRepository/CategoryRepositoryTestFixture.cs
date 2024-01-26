d95bac792e36b343f3396cf7b3acf1edd4a7b32a
leandro4silva <leandro94.a.s@gmail.com>
2024-01-26 16:22:49 -0300

    feat: implements tests sort into category repository search method

--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,4 +1,5 @@
 ﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 using FC.Codeflix.Catalog.Infra.Data.EF;
 using FC.Codeflix.Catalog.IntegrationTests.Common;
 using Microsoft.EntityFrameworkCore;
@@ -79,6 +80,27 @@ public class CategoryRepositoryTestFixture : BaseFixture
             category.Update(name, category.Description);
             return category;
         }).ToList();
+    }
+
+    public List<Category> CloneCategoriesListOrdered(
+        List<Category> categoriesList, 
+        string orderBy, 
+        SearchOrder order
+    )
+    {
+        var listClone = new List<Category>(categoriesList);
+        var orderedEnumerable = (orderBy.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => listClone.OrderBy(x => x.Name),
+            ("name", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Name),
+            ("id", SearchOrder.Asc) => listClone.OrderBy(x => x.Id),
+            ("id", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Id),
+            ("createdat", SearchOrder.Asc) => listClone.OrderBy(x => x.CreatedAt),
+            ("createdat", SearchOrder.Desc) => listClone.OrderByDescending(x => x.CreatedAt),
+            _ => listClone.OrderBy(x => x.Name)
+        };
+
+        return orderedEnumerable.ToList();
     }
 }
 
