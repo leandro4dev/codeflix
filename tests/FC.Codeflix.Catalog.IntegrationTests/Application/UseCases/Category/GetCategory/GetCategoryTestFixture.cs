030d350736eeb125defa9149b5b5e7c263e1d3bd
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-28 12:34:44 -0300

    feat: implements get category integration test

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+
+public class GetCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+        
+
+}
+
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureColletion 
+    : ICollectionFixture<GetCategoryTestFixture> 
+{ }
