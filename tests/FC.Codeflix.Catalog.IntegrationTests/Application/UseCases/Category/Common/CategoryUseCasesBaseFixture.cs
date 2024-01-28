d7ad522d6187f429ccfd36acfbab4bb6451dba94
Leandro A. Silva <leandro94.a.s@gmail.com>
2024-01-28 12:34:00 -0300

    feat: create cateogry use case base fixture

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/Common/CategoryUseCasesBaseFixture.cs
@@ -0,0 +1,55 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Common;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+public class CategoryUseCasesBaseFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName[..255]; //primeiros 255 characters
+        }
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10000)
+        {
+            categoryDescription = categoryDescription[..10_000];
+        }
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean()
+    {
+        return new Random().NextDouble() < 0.5;
+    }
+
+    public DomainEntity.Category GetExampleCategory()
+    {
+        return new DomainEntity.Category(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+    }
+
+    public List<DomainEntity.Category> GetExampleCategoriesList(int length = 10)
+    {
+        return Enumerable.Range(1, length)
+            .Select(_ => GetExampleCategory()).ToList();
+    }
+}
