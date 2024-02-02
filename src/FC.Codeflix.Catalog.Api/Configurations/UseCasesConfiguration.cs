a477cf8c25aec84828a0845ed4b08bb8f61bc80f
leandro4silva <leandro94.a.s@gmail.com>
2024-02-02 11:50:37 -0300

    feat: setup api

---
+++ src/FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using FC.Codeflix.Catalog.Infra.Data.EF.UnitOfWork;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class UseCasesConfiguration
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(CreateCategory));
+        services.AddRepository();
+        return services;
+    }
+
+    private static IServiceCollection AddRepository(this IServiceCollection services)
+    {
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
