15e2103fdbe75cb92614e2f1847f1a728fbfdcdd
leandro4silva <leandro94.a.s@gmail.com>
2024-02-06 15:38:28 -0300

    fix: semantic on error when cant instantiate aggregate

--- tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -165,7 +165,7 @@ public class UpdateCategoryApiTest
         );
 
         response.Should().NotBeNull();
-        response!.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+        response!.StatusCode.Should().Be((HttpStatusCode) StatusCodes.Status422UnprocessableEntity);
         output.Should().NotBeNull();
         output!.Title.Should().Be("One or more validation errors ocurred");
         output.Type.Should().Be("UnprocessableEntity");
